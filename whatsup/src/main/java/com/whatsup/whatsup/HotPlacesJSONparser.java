d98da73e22820a4816a25a8893b3a25b692ebc74
leninbooter <leninbooter@gmail.com>
2014-06-12 15:25:55 -0430

    Fullness Images
    from hotplaces

--- whatsup/src/main/java/com/whatsup/whatsup/HotPlacesJSONparser.java
+++ whatsup/src/main/java/com/whatsup/whatsup/HotPlacesJSONparser.java
@@ -47,14 +47,55 @@ public class HotPlacesJSONparser {
     private HashMap<String, Object> getPlace(JSONObject jPlace) {
         Log.d("Hot place parser getPlace: ","entrada");
         HashMap<String, Object> place = new HashMap<String, Object>();
+        Float fullness, capacity;
 
         try {
             place.put("name", jPlace.getString("name"));
             place.put("gp_formatted_address", jPlace.getString("gp_formatted_address"));
             place.put("gp_icon", R.drawable.blank );
             place.put("gp_icon_path", jPlace.getString("gp_icon"));
-            place.put("fullness", jPlace.getString("fullness"));
-            place.put("capacity", jPlace.getString("capacity"));
+            fullness = Float.valueOf( jPlace.getString("fullness") );
+            capacity = Float.valueOf( jPlace.getString("capacity") );
+            fullness = (fullness / capacity) * 100;
+            Log.d("Fullness: ", String.valueOf(fullness));
+            if( fullness >= 0.0 && fullness <= 10.0 ) {
+                place.put("fullness", R.drawable.ten);
+            }else {
+                if( fullness >= 11.0 && fullness <= 20.0 ) {
+                    place.put("fullness", R.drawable.twenty);
+                }else {
+                    if( fullness >= 21.0 && fullness <= 30.0 ) {
+                        place.put("fullness", R.drawable.thirty);
+                    }else {
+                        if( fullness >= 31.0 && fullness <= 40.0 ) {
+                            place.put("fullness", R.drawable.fourty);
+                        }else {
+                            if( fullness >= 41.0 && fullness <= 50.0 ) {
+                                place.put("fullness", R.drawable.fifty);
+                            }else {
+                                if( fullness >= 51.0 && fullness <= 60.0 ) {
+                                    place.put("fullness", R.drawable.sixty);
+                                }else {
+                                    if( fullness >= 61.0 && fullness <= 70.0 ) {
+                                        place.put("fullness", R.drawable.seventy);
+                                    }else {
+                                        if( fullness >= 71.0 && fullness <= 80.0 ) {
+                                            place.put("fullness", R.drawable.eighty);
+                                        }else {
+                                            if( fullness >= 81.0 && fullness <= 90.0 ) {
+                                                place.put("fullness", R.drawable.ninety);
+                                            }else {
+                                                place.put("fullness", R.drawable.hundred);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
         }catch (JSONException e ) {
             e.printStackTrace();
         }
